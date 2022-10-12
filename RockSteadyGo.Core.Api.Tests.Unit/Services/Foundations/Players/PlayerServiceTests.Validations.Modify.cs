5d730221c80e204044a8df9198a8cd06ee7ce298
Christo du Toit <christo@dutoit.co.uk>
2022-10-13 00:20:59 +0100

    ShouldThrowValidationExceptionOnModifyIfPlayerIsNullAndLogItAsync -> FAIL

---
+++ RockSteadyGo.Core.Api.Tests.Unit/Services/Foundations/Players/PlayerServiceTests.Validations.Modify.cs
@@ -0,0 +1,57 @@
+// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using RockSteadyGo.Core.Api.Models.Players;
+using RockSteadyGo.Core.Api.Models.Players.Exceptions;
+using Xunit;
+
+namespace RockSteadyGo.Core.Api.Tests.Unit.Services.Foundations.Players
+{
+    public partial class PlayerServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfPlayerIsNullAndLogItAsync()
+        {
+            // given
+            Player nullPlayer = null;
+            var nullPlayerException = new NullPlayerException();
+
+            var expectedPlayerValidationException =
+                new PlayerValidationException(nullPlayerException);
+
+            // when
+            ValueTask<Player> modifyPlayerTask =
+                this.playerService.ModifyPlayerAsync(nullPlayer);
+
+            PlayerValidationException actualPlayerValidationException =
+                await Assert.ThrowsAsync<PlayerValidationException>(
+                    modifyPlayerTask.AsTask);
+
+            // then
+            actualPlayerValidationException.Should()
+                .BeEquivalentTo(expectedPlayerValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPlayerValidationException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdatePlayerAsync(It.IsAny<Player>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
