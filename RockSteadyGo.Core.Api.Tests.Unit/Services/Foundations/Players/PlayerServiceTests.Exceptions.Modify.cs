51fd30660ab97265802fadc08f681f3e6be86022
Christo du Toit <christo@dutoit.co.uk>
2022-10-13 00:44:05 +0100

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ RockSteadyGo.Core.Api.Tests.Unit/Services/Foundations/Players/PlayerServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,69 @@
+// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using RockSteadyGo.Core.Api.Models.Players;
+using RockSteadyGo.Core.Api.Models.Players.Exceptions;
+using Xunit;
+
+namespace RockSteadyGo.Core.Api.Tests.Unit.Services.Foundations.Players
+{
+    public partial class PlayerServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Player randomPlayer = CreateRandomPlayer();
+            SqlException sqlException = GetSqlException();
+
+            var failedPlayerStorageException =
+                new FailedPlayerStorageException(sqlException);
+
+            var expectedPlayerDependencyException =
+                new PlayerDependencyException(failedPlayerStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(sqlException);
+
+            // when
+            ValueTask<Player> modifyPlayerTask =
+                this.playerService.ModifyPlayerAsync(randomPlayer);
+
+            PlayerDependencyException actualPlayerDependencyException =
+                await Assert.ThrowsAsync<PlayerDependencyException>(
+                    modifyPlayerTask.AsTask);
+
+            // then
+            actualPlayerDependencyException.Should()
+                .BeEquivalentTo(expectedPlayerDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPlayerByIdAsync(randomPlayer.Id),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedPlayerDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdatePlayerAsync(randomPlayer),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
