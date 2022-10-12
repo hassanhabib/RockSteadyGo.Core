cf94c505d21ba31ab24cd2a405152bd812af5199
Christo du Toit <christo@dutoit.co.uk>
2022-10-12 23:48:46 +0100

    ShouldRetrievePlayerByIdAsync -> PASS

--- RockSteadyGo.Core.Api/Services/Foundations/Players/PlayerService.cs
+++ RockSteadyGo.Core.Api/Services/Foundations/Players/PlayerService.cs
@@ -40,7 +40,7 @@ namespace RockSteadyGo.Core.Api.Services.Foundations.Players
         public IQueryable<Player> RetrieveAllPlayers() =>
             TryCatch(() => this.storageBroker.SelectAllPlayers());
 
-        public ValueTask<Player> RetrievePlayerByIdAsync(Guid playerId) =>
-            throw new NotImplementedException();
+        public async ValueTask<Player> RetrievePlayerByIdAsync(Guid playerId) =>
+            await this.storageBroker.SelectPlayerByIdAsync(playerId);
     }
 }
