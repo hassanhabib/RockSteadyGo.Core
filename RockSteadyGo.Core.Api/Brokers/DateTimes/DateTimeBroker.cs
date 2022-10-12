d2f038e82459bbff0b3157bebd02909f07aaf5e4
Christo du Toit <christo@dutoit.co.uk>
2022-10-12 17:54:00 +0100

    BROKERS: DateTime - Get Current DateTime Offset

---
+++ RockSteadyGo.Core.Api/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace RockSteadyGo.Core.Api.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTimeOffset() =>
+            DateTimeOffset.UtcNow;
+    }
+}
