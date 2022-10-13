742f10826c761d3d025f1d9230ad499db47ef149
Christo du Toit <christo@dutoit.co.uk>
2022-10-14 00:13:19 +0100

    CODE RUB: Change EF Adaptation

--- RockSteadyGo.Core.Api/Brokers/Storages/StorageBroker.Players.cs
+++ RockSteadyGo.Core.Api/Brokers/Storages/StorageBroker.Players.cs
@@ -7,7 +7,6 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using RockSteadyGo.Core.Api.Models.Players;
 
 namespace RockSteadyGo.Core.Api.Brokers.Storages
@@ -16,59 +15,18 @@ namespace RockSteadyGo.Core.Api.Brokers.Storages
     {
         public DbSet<Player> Players { get; set; }
 
-        public async ValueTask<Player> InsertPlayerAsync(Player player)
-        {
-            using var broker =
-                new StorageBroker(this.configuration);
+        public async ValueTask<Player> InsertPlayerAsync(Player player) =>
+            await InsertAsync(player);
 
-            EntityEntry<Player> playerEntityEntry =
-                await broker.Players.AddAsync(player);
+        public IQueryable<Player> SelectAllPlayers() => SelectAll<Player>();
 
-            await broker.SaveChangesAsync();
+        public async ValueTask<Player> SelectPlayerByIdAsync(Guid playerId) =>
+            await SelectAsync<Player>(playerId);
 
-            return playerEntityEntry.Entity;
-        }
+        public async ValueTask<Player> UpdatePlayerAsync(Player player) =>
+            await UpdateAsync(player);
 
-        public IQueryable<Player> SelectAllPlayers()
-        {
-            using var broker =
-                new StorageBroker(this.configuration);
-
-            return broker.Players;
-        }
-
-        public async ValueTask<Player> SelectPlayerByIdAsync(Guid playerId)
-        {
-            using var broker =
-                new StorageBroker(this.configuration);
-
-            return await broker.Players.FindAsync(playerId);
-        }
-
-        public async ValueTask<Player> UpdatePlayerAsync(Player player)
-        {
-            using var broker =
-                new StorageBroker(this.configuration);
-
-            EntityEntry<Player> playerEntityEntry =
-                broker.Players.Update(player);
-
-            await broker.SaveChangesAsync();
-
-            return playerEntityEntry.Entity;
-        }
-
-        public async ValueTask<Player> DeletePlayerAsync(Player player)
-        {
-            using var broker =
-                new StorageBroker(this.configuration);
-
-            EntityEntry<Player> playerEntityEntry =
-                broker.Players.Remove(player);
-
-            await broker.SaveChangesAsync();
-
-            return playerEntityEntry.Entity;
-        }
+        public async ValueTask<Player> DeletePlayerAsync(Player player) =>
+            await DeleteAsync(player);
     }
 }
