926f7786675f1f4fd9ed816e36f137b54a9089ed
Christo du Toit <christo@dutoit.co.uk>
2022-10-14 09:44:18 +0100

    CODE RUB: Amended Model

--- RockSteadyGo.Core.Api.Tests.Acceptance/Models/Players/Player.cs
+++ RockSteadyGo.Core.Api.Tests.Acceptance/Models/Players/Player.cs
@@ -1,3 +1,8 @@
+// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
 using System;
 
 namespace RockSteadyGo.Core.Api.Tests.Acceptance.Models.Players
@@ -5,12 +10,8 @@ namespace RockSteadyGo.Core.Api.Tests.Acceptance.Models.Players
     public class Player
     {
         public Guid Id { get; set; }
-
-        // TODO:  Add your properties here
-
-        public Guid CreatedByUserId { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
-        public Guid UpdatedByUserId { get; set; }
-        public DateTimeOffset UpdatedDate { get; set; }
     }
 }
