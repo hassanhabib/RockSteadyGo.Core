6b0aad512cd28ccf3a14c32bbe322eae876aa317
Christo du Toit <christo@dutoit.co.uk>
2022-10-14 21:58:56 +0100

    ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> PASS

--- RockSteadyGo.Core.Api.Tests.Unit/Services/Foundations/Matches/MatchServiceTests.Exceptions.RemoveById.cs
+++ RockSteadyGo.Core.Api.Tests.Unit/Services/Foundations/Matches/MatchServiceTests.Exceptions.RemoveById.cs
@@ -3,9 +3,11 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using RockSteadyGo.Core.Api.Models.Matches.Exceptions;
 using Xunit;
@@ -59,6 +61,55 @@ namespace RockSteadyGo.Core.Api.Tests.Unit.Services.Foundations.Matches
 
             this.dateTimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someMatchId = Guid.NewGuid();
+
+            var databaseUpdateConcurrencyException =
+                new DbUpdateConcurrencyException();
+
+            var lockedMatchException =
+                new LockedMatchException(databaseUpdateConcurrencyException);
+
+            var expectedMatchDependencyValidationException =
+                new MatchDependencyValidationException(lockedMatchException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectMatchByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<Match> removeMatchByIdTask =
+                this.matchService.RemoveMatchByIdAsync(someMatchId);
+
+            MatchDependencyValidationException actualMatchDependencyValidationException =
+                await Assert.ThrowsAsync<MatchDependencyValidationException>(
+                    removeMatchByIdTask.AsTask);
+
+            // then
+            actualMatchDependencyValidationException.Should()
+                .BeEquivalentTo(expectedMatchDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectMatchByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMatchDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteMatchAsync(It.IsAny<Match>()),
                     Times.Never);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
