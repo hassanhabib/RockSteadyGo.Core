5707740fce82fc8cbaf13c422dc3cee825fcf345
Christo du Toit <christo@dutoit.co.uk>
2022-10-15 15:15:47 +0100

    ShouldThrowValidationExceptionOnModifyIfMoveIsInvalidAndLogItAsync -> FAIL

--- RockSteadyGo.Core.Api.Tests.Unit/Services/Foundations/Moves/MoveServiceTests.Validations.Modify.cs
+++ RockSteadyGo.Core.Api.Tests.Unit/Services/Foundations/Moves/MoveServiceTests.Validations.Modify.cs
@@ -53,5 +53,75 @@ namespace RockSteadyGo.Core.Api.Tests.Unit.Services.Foundations.Moves
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(3)]
+        public async Task ShouldThrowValidationExceptionOnModifyIfMoveIsInvalidAndLogItAsync(int invalidLocation)
+        {
+            // given 
+            var invalidMove = new Move();
+            invalidMove.LocationX = invalidLocation;
+            invalidMove.LocationY = invalidLocation;
+
+            var invalidMoveException =
+                new InvalidMoveException();
+
+            invalidMoveException.AddData(
+                key: nameof(Move.Id),
+                values: "Id is required");
+
+            invalidMoveException.AddData(
+                key: nameof(Move.MatchId),
+                values: "Id is required");
+
+            invalidMoveException.AddData(
+                key: nameof(Move.PlayerId),
+                values: "Id is required");
+
+            invalidMoveException.AddData(
+                key: nameof(Move.LocationX),
+                values: "Value is required");
+
+            invalidMoveException.AddData(
+                key: nameof(Move.LocationY),
+                values: "Value is required");
+
+            invalidMoveException.AddData(
+                key: nameof(Move.CreatedDate),
+                values: "Date is required");
+
+            var expectedMoveValidationException =
+                new MoveValidationException(invalidMoveException);
+
+            // when
+            ValueTask<Move> modifyMoveTask =
+                this.moveService.ModifyMoveAsync(invalidMove);
+
+            MoveValidationException actualMoveValidationException =
+                await Assert.ThrowsAsync<MoveValidationException>(
+                    modifyMoveTask.AsTask);
+
+            //then
+            actualMoveValidationException.Should()
+                .BeEquivalentTo(expectedMoveValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMoveValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateMoveAsync(It.IsAny<Move>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
