dc7f691f900e47f5fcef231a2f77221d8ce5c896
Christo du Toit <christo@dutoit.co.uk>
2022-10-15 13:08:22 +0100

    ShouldThrowValidationExceptionOnAddIfMoveIsInvalidAndLogItAsync -> FAIL

--- RockSteadyGo.Core.Api.Tests.Unit/Services/Foundations/Moves/MoveServiceTests.Validations.Add.cs
+++ RockSteadyGo.Core.Api.Tests.Unit/Services/Foundations/Moves/MoveServiceTests.Validations.Add.cs
@@ -1,7 +1,6 @@
 // ---------------------------------------------------------------
-// Copyright (c) Christo du Toit. All rights reserved.
-// Licensed under the MIT License.
-// See License.txt in the project root for license information.
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
 using System.Threading.Tasks;
@@ -47,6 +46,76 @@ namespace RockSteadyGo.Core.Api.Tests.Unit.Services.Foundations.Moves
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfMoveIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidMove = new Move();
+
+            var invalidMoveException =
+                new InvalidMoveException();
+
+            invalidMoveException.AddData(
+                key: nameof(Move.Id),
+                values: "Id is required");
+
+            invalidMoveException.AddData(
+                key: nameof(Move.MatchId),
+                values: "Id is required");
+
+            invalidMoveException.AddData(
+                key: nameof(Move.PlayerId),
+                values: "Id is required");
+
+            invalidMoveException.AddData(
+                key: nameof(Move.LocationX),
+                values: "Value is required");
+
+            invalidMoveException.AddData(
+                key: nameof(Move.LocationY),
+                values: "Value is required");
+
+            invalidMoveException.AddData(
+                key: nameof(Move.Type),
+                values: "Value is required");
+
+            invalidMoveException.AddData(
+                key: nameof(Move.CreatedDate),
+                values: "Date is required");
+
+            var expectedMoveValidationException =
+                new MoveValidationException(invalidMoveException);
+
+            // when
+            ValueTask<Move> addMoveTask =
+                this.moveService.AddMoveAsync(invalidMove);
+
+            MoveValidationException actualMoveValidationException =
+                await Assert.ThrowsAsync<MoveValidationException>(() =>
+                    addMoveTask.AsTask());
+
+            // then
+            actualMoveValidationException.Should()
+                .BeEquivalentTo(expectedMoveValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMoveValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertMoveAsync(It.IsAny<Move>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Never());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
