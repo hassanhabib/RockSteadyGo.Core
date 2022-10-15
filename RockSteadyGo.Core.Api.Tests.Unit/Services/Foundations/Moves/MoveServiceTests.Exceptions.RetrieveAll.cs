6dfcddb3c62cf44d86f526d2f6e70addfe726885
Christo du Toit <christo@dutoit.co.uk>
2022-10-15 14:36:39 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ RockSteadyGo.Core.Api.Tests.Unit/Services/Foundations/Moves/MoveServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,58 @@
+// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using RockSteadyGo.Core.Api.Models.Moves.Exceptions;
+using Xunit;
+
+namespace RockSteadyGo.Core.Api.Tests.Unit.Services.Foundations.Moves
+{
+    public partial class MoveServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedStorageException =
+                new FailedMoveStorageException(sqlException);
+
+            var expectedMoveDependencyException =
+                new MoveDependencyException(failedStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllMoves())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllMovesAction = () =>
+                this.moveService.RetrieveAllMoves();
+
+            MoveDependencyException actualMoveDependencyException =
+                Assert.Throws<MoveDependencyException>(retrieveAllMovesAction);
+
+            // then
+            actualMoveDependencyException.Should()
+                .BeEquivalentTo(expectedMoveDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllMoves(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedMoveDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
